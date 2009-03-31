a8048ac290cabce10a4186f46d0ad28b137117f5
JD Huntington <jdhuntington@gmail.com>
2009-03-30 18:36:19 -0700

    Draw a hexagon on a <canvas>

---
+++ research/canvas.js
@@ -0,0 +1,55 @@
+Event.observe(window, 'load', function() {
+		M.initialize();
+		MapManager.initialize();
+		MapManager.addTile();
+});
+
+M = {				// measurements
+  s: 60,
+  initialize: function()
+  {
+    this.h = Math.sin(Math.PI * 1/6.0) * this.s;
+    this.r = Math.cos(Math.PI * 1/6.0) * this.s;
+    this.height = this.s + (2 * this.h);
+    this.width = 2 * this.r;
+    this.bv = this.s + this.h;
+  }
+};
+
+MapManager = {
+  tiles: $A(),
+  initialize: function()
+  {
+    this.map = $('map');
+  },
+  addTile: function()
+  {
+    var tile = document.createElement('canvas');
+
+    tile.setAttribute('width', 200);
+    tile.setAttribute('height', 200);
+    tile.setStyle({
+		    position: 'absolute',
+		    top: '90px',
+		    left: '390px'
+		  });
+    this.drawTile(tile);
+    this.map.appendChild(tile);
+    this.tiles[this.tiles.length] = tile;
+  },
+  drawTile: function(tile)
+  {
+    var ctx = tile.getContext("2d");
+    ctx.fillStyle = '#ffff00';
+    ctx.beginPath();
+    ctx.moveTo(M.r, 0);
+    ctx.lineTo(M.width, M.h);
+    ctx.lineTo(M.width, M.bv);
+    ctx.lineTo(M.r, M.height);
+    ctx.lineTo(0, M.bv);
+    ctx.lineTo(0, M.h);
+    ctx.lineTo(M.r, 0);
+    ctx.fill();
+    ctx.closePath();
+  }
+};
