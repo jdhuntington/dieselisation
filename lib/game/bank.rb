bd96626f4f1f7061a32e441a3a8db418ff60747e
JD Huntington <jdhuntington@gmail.com>
2009-04-05 21:16:58 -0700

    bank#balance

--- lib/game/bank.rb
+++ lib/game/bank.rb
@@ -2,4 +2,10 @@ class Bank
   def self.instance
     @bank ||= Bank.new
   end
+
+  attr_reader :balance
+
+  def init(params)
+    @balance = params[:balance]
+  end
 end
--- spec/lib/bank_spec.rb
+++ spec/lib/bank_spec.rb
@@ -5,4 +5,9 @@ describe Bank do
   it 'should be a singleton' do # Is this enforceable in ruby?
     Bank.instance.should == Bank.instance
   end
+
+  it 'should have a balance' do
+    Bank.instance.init(:balance => 99)
+    Bank.instance.balance.should == 99
+  end
 end
