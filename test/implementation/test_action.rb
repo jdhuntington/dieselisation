1b0b5ccbf872c30ff63a583442e4bbd1023b5c5e
JD Huntington <jdhuntington@gmail.com>
2009-09-06 10:08:05 -0700

    Update test to reflect name change

--- test/implementation/test_action.rb
+++ test/implementation/test_action.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../implementation_test_helper'
 
-ActionStub = Struct.new(:name, :par)
+ActionStub = Struct.new(:type, :par)
 class TestAction < Test::Unit::TestCase
   def test_buy_private_action_should_describe_itself
     private = ActionStub.new('foobar', 9)
