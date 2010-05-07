726d4b9b603581b5e4465d6c284cf10952a4a1f6
JD Huntington <jd@aboutus.org>
2010-05-06 20:42:33 -0700

    Make xml -> yml script a little more portable

--- script/import_rails.rb
+++ script/import_rails.rb
@@ -4,40 +4,31 @@ require 'yaml'
 require 'pp'
 
 game      = "18AL"
-src_dir   = "../raw_data/#{game}"
-data_dir  = "../lib/dieselisation/#{game}"
+src_dir   = "#{RAILS_ROOT}/raw_data/#{game}"
+data_dir  = "#{RAILS_ROOT}/lib/dieselisation/#{game}"
 
 unless File.exists?(data_dir)
   Dir.mkdir(data_dir)
 end
 
-Dir.entries(src_dir).each do |f|
-  if f =~ /.xml/
-    xml = File.read("#{src_dir}/#{f}")
-    raw = XmlSimple.xml_in(xml)
-    # pp raw
-    # puts raw.to_yaml
-    @out_file = "#{data_dir}/#{f.gsub('.xml', '.yml')}"
-    open(@out_file, 'w') {|file| YAML.dump(raw, file)}
-    
-    # break
-  end
+Dir.glob("#{src_dir}/*.xml").each do |f|
+  f = File.expand_path(f)
+  xml = File.read(f)
+  raw = XmlSimple.xml_in(xml)
+  out_file = f.gsub(/\.xml$/, '.yml')
+  open(out_file, 'w') {|file| YAML.dump(raw, file)}
 end
 
 ### now gather all the data and stuff it into one file
-merged_file = "#{data_dir}/game_#{game}.yml"
+merged_file = File.expand_path("#{data_dir}/game_#{game}.yml")
 if File.exist?(merged_file)
   File.delete(merged_file)
 end
 
 merged_hash = {}
-Dir.entries(data_dir).each do |f|
-  if f =~ /.yml/
-    merged_hash = merged_hash.merge(YAML.load_file("#{data_dir}/#{f}"))
-  end
+Dir.glob("#{src_dir}/*.yml").each do |f|
+  f = File.expand_path(f)
+  merged_hash = merged_hash.merge(YAML.load_file(f))
 end
 open(merged_file, 'w') {|file| YAML.dump(merged_hash, file)}
 
-merged_hash["Company"].each do |c|
-  pp "#{c['name']} - #{c['type']}"
-end
